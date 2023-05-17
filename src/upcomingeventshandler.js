0c315f1ab77a0979a93b603aa6b1488e45bb301a
pickingname <trintrinir@icloud.com>
2023-05-17 23:57:31 +0000

    fixes patch: 4

--- src/upcomingeventshandler.js
+++ src/upcomingeventshandler.js
@@ -2,10 +2,11 @@ import { events } from './eventshandler.js';
 
 const futureEventsDiv = document.getElementById("futureevents");
 
-const futureEvents = events.filter(event => event.date > new Date());
+const currentDate = new Date();
 
-console.log("Current Date:", new Date());
+const futureEvents = events.filter(event => event.date > currentDate);
 
+console.log("Current Date:", currentDate);
 console.log("Future Events:", futureEvents);
 
 futureEvents.sort((a, b) => a.date - b.date);
@@ -15,7 +16,7 @@ if (futureEvents.length > 0) {
   const eventsToShow = futureEvents.slice(0, numberOfEventsToShow);
 
   eventsToShow.forEach(event => {
-    const countdown = Math.ceil((event.date - new Date()) / (1000 * 60 * 60 * 24));
+    const countdown = Math.ceil((event.date - currentDate) / (1000 * 60 * 60 * 24));
 
     const eventSpan = document.createElement("span");
     eventSpan.innerHTML = `${event.name}, <code>${event.date.toLocaleDateString()}</code> (In: ${countdown}d)`;
