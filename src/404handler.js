a6bfe05e48e0c8dc878908ff0650a34990f4a18b
pickingname <trintrinir@icloud.com>
2023-05-19 20:53:10 +0700

    add some 404 messages and rotations

--- src/404handler.js
+++ src/404handler.js
@@ -1,16 +1,47 @@
 // List of messages
 const messages = [
     "What?",
-    "How did we get here?",
+    "<p class='text-purple-500'>How did we get here?</p>",
     "Not found",
-    "Invalid request"
+    "Invalid request",
+    "Nah, this website is way too hard",
+    "GET http://localhost/[object%20Event] 404 (Not Found)",
+    "Uh oh"
   ];
   
-  // Select a random message
-  const randomMessage = messages[Math.floor(Math.random() * messages.length)];
-  
-  // Get the element with the ID "randommessage"
   const randomMessageElement = document.getElementById("randommessage");
   
-  // Append the random message to the element
-  randomMessageElement.textContent = randomMessage;  
+  let shuffledMessages = messages.slice();
+  shuffleArray(shuffledMessages);
+  
+  let messageIndex = 0;
+  randomMessageElement.innerHTML = shuffledMessages[messageIndex];
+  
+  function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+  }
+  
+  function transitionRandomMessage() {
+    randomMessageElement.style.opacity = 0;
+  
+    setTimeout(() => {
+      messageIndex++;
+  
+      if (messageIndex === shuffledMessages.length) {
+        // Reshuffle the messages array
+        shuffleArray(shuffledMessages);
+        messageIndex = 0;
+      }
+  
+      const randomMessage = shuffledMessages[messageIndex];
+  
+      randomMessageElement.innerHTML = randomMessage;
+  
+      randomMessageElement.style.opacity = 1;
+    }, 500);
+  }
+  
+  setInterval(transitionRandomMessage, 10000);
