c8d301d11b5c23232681ad9f910fd4e2639d8360
pickingname <trintrinir@icloud.com>
2023-05-21 10:14:57 +0700

    hw charts Core

---
+++ src/hw-chart.js
@@ -0,0 +1,66 @@
+export const daliyHomeworks = [1, 3, 0, 1, 1];
+
+const data = {
+  labels: ["Day 1", "Day 2", "Day 3", "Day 4", "Day 5"],
+  datasets: [
+    {
+      label: "Homeworks per Day",
+      backgroundColor: "hsl(259, 61%, 87%)",
+      borderColor: "hsl(259, 61%, 87%)",
+      data: daliyHomeworks,
+      fill: false
+    },
+  ],
+};
+
+const configLineChart = {
+  type: "line",
+  data: data,
+  options: {
+    maintainAspectRatio: false,
+    responsive: true,
+    legend: {
+      display: true,
+      labels: {
+        fontColor: 'white',
+        fontSize: 14
+      }
+    },
+    scales: {
+      x: {
+        grid: {
+          display: true
+        },
+        ticks: {
+          fontColor: 'black',
+          fontSize: 12,
+          maxTicksLimit: 8
+        }
+      },
+      y: {
+        grid: {
+          display: true
+        },
+        ticks: {
+          fontColor: 'black',
+          fontSize: 12,
+          beginAtZero: true
+        }
+      }
+    },
+    tooltips: {
+      mode: 'index',
+      intersect: false,
+      callbacks: {
+        label: function(tooltipItem, data) {
+          return tooltipItem.yLabel.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+        }
+      }
+    }
+  },
+};
+
+var chartLine = new Chart(
+  document.getElementById("hwchart"),
+  configLineChart
+);
